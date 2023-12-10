07cc4ffccde69b720eb4898a9b5451907920648f
Aaron Jordan <aaron@impactfulmedia.co>
2023-12-10 17:30:05 -0600

    🚧 added sample luaunit tests to exercise 1

--- lesson-1/exercises/exercise_01.lua
+++ lesson-1/exercises/exercise_01.lua
@@ -1,4 +1,19 @@
 -- Sample script that doesn't really do anything to test CI integration.
+local lpeg = require "lpeg"
+local lu = require "luaunit"
 
-local test = "testing"
+local test = "testing 👷"
 print(test);
+
+local result = 1 + 2
+
+function TestBasicMath()
+  lu.assertEquals(result, 3)
+end
+
+function TestTestMessage()
+  lu.assertStrContains(test, 'test')
+  lu.assertStrContains(test, '👷')
+end
+
+os.exit(lu.LuaUnit.run())
